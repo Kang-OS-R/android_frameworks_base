79b09c08e3ddbd4a3bd3d36f4a279be7dccdb4ef
Gianmarco Reverberi <gianmarco.reverberi@gmail.com>
2015-03-16 20:02:15 +0100

    SystemUpdateService: enable service but lock its receivers
    Added a check for ensure that disabled components are not
    re-enabled at runtime
    
    Added code for forcing enable of previously disabled components
    
    Change-Id: Icfcfa26ccb85028d32edbb5cdb3dd7cdae85b720
    [jaysonedson@gmail.com: Fixed hasComponentClassName]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -3439,6 +3439,40 @@ public class PackageManagerService extends IPackageManager.Stub
                 }
             }
 
+            // Disable components marked for disabling at build-time
+            mDisabledComponentsList = new ArrayList<ComponentName>();
+            for (String name : mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_disabledComponents)) {
+                ComponentName cn = ComponentName.unflattenFromString(name);
+                mDisabledComponentsList.add(cn);
+                Slog.v(TAG, "Disabling " + name);
+                String className = cn.getClassName();
+                PackageSetting pkgSetting = mSettings.mPackages.get(cn.getPackageName());
+                AndroidPackage pkg = pkgSetting.pkg;
+                if (pkgSetting == null || pkgSetting.pkg == null
+                        || !AndroidPackageUtils.hasComponentClassName(pkg, className)) {
+                    Slog.w(TAG, "Unable to disable " + name);
+                    continue;
+                }
+                pkgSetting.disableComponentLPw(className, UserHandle.USER_OWNER);
+            }
+
+            // Enable components marked for forced-enable at build-time
+            for (String name : mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_forceEnabledComponents)) {
+                ComponentName cn = ComponentName.unflattenFromString(name);
+                Slog.v(TAG, "Enabling " + name);
+                String className = cn.getClassName();
+                PackageSetting pkgSetting = mSettings.mPackages.get(cn.getPackageName());
+                AndroidPackage pkg = pkgSetting.pkg;
+                if (pkgSetting == null || pkgSetting.pkg == null
+                        || !AndroidPackageUtils.hasComponentClassName(pkg, className)) {
+                    Slog.w(TAG, "Unable to enable " + name);
+                    continue;
+                }
+                pkgSetting.enableComponentLPw(className, UserHandle.USER_OWNER);
+            }
+
             // Prepare storage for system user really early during boot,
             // since core system apps like SettingsProvider and SystemUI
             // can't wait for user to start
