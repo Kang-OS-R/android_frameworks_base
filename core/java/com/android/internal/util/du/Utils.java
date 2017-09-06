500398749b70af731a7eceb2fec79286d03dceac
bigrushdog <randall.rushing@gmail.com>
2017-09-06 13:09:52 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to Utils
    adapted for partial screenshot
    
    Change-Id: Ie91990fb3c4ddb5f5e7e45f617c57b1ffa1526b8

--- core/java/com/android/internal/util/du/Utils.java
+++ core/java/com/android/internal/util/du/Utils.java
@@ -71,6 +71,9 @@ public class Utils {
 
     private static OverlayManager mOverlayService;
 
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+
 	// Check if device is connected to Wi-Fi
     public static boolean isWiFiConnected(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -98,6 +101,14 @@ public class Utils {
         return telephony != null && telephony.isVoiceCapable();
     }
 
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
 	// Check if device is connected to the internet
     public static boolean isConnected(Context context) {
