250b40d8d7eb1c2d20bb7049a905ea44c755014b
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Long press tile to change between Style (dark/light/auto ui)
    mode and Accent (#FruityPebbles) mode.
    Click to show detail view.
    
    Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5
    
     Conflicts:
    	packages/SystemUI/res/values/du_strings.xml
    	packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
    
    Change-Id: Ifa1e1cb6f92afbfa2fd2839b1c7586a06be5c339
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -41,6 +41,10 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ThemeTile;
+import com.android.systemui.qs.tiles.UiModeNightTile;
+import com.android.systemui.qs.tiles.SyncTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -181,6 +185,12 @@ public class QSFactoryImpl implements QSFactory {
                 return mScreenRecordTileProvider.get();
             case "aod":
                 return mAODTileProvider.get();
+            case "hwkeys":
+                return mHWKeysTileProvider.get();
+            case "dataswitch":
+                return mDataSwitchTileProvider.get();
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Custom tiles
