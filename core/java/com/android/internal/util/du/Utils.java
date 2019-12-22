4b272c403030e0cc79b4ae3a6db45b0d1348324c
Alex Cruz <du.alexcruz@gmail.com>
2019-12-22 14:39:55 -0500

    Fruity Pebbles: Add method to detect whether overlay is enabled
    Change-Id: Ibd4ecb00ceed29604fdcd5d2822edcdb3ac399e2
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/com/android/internal/util/du/Utils.java
+++ core/java/com/android/internal/util/du/Utils.java
@@ -18,6 +18,9 @@ package com.android.internal.util.du;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -26,6 +29,7 @@ import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.input.InputManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.UserHandle;
 import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.os.SystemClock;
@@ -45,8 +49,13 @@ import android.view.KeyEvent;
 import com.android.internal.R;
 
 import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
+
+
+    private static OverlayManager mOverlayService;
 
     // Check to see if device is WiFi only
     public static boolean isWifiOnly(Context context) {
@@ -169,5 +178,41 @@ public class Utils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    // Method to detect whether an overlay is enabled or not
+    public static boolean isThemeEnabled(String packageName) {
+        mOverlayService = new OverlayManager();
+        try {
+            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
+                    UserHandle.myUserId());
+            for (int i = 0, size = infos.size(); i < size; i++) {
+                if (infos.get(i).packageName.equals(packageName)) {
+                    return infos.get(i).isEnabled();
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static class OverlayManager {
+        private final IOverlayManager mService;
+
+        public OverlayManager() {
+            mService = IOverlayManager.Stub.asInterface(
+                    ServiceManager.getService(Context.OVERLAY_SERVICE));
+        }
+
+        public void setEnabled(String pkg, boolean enabled, int userId)
+                throws RemoteException {
+            mService.setEnabled(pkg, enabled, userId);
+        }
+
+        public List<OverlayInfo> getOverlayInfosForTarget(String target, int userId)
+                throws RemoteException {
+            return mService.getOverlayInfosForTarget(target, userId);
+        }
     }
 }
