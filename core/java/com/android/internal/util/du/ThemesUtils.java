bdb9a34599fdb5c37a1c85db84fe616c53f6a7bd
RakeshBatra <Raakesh.Batra@rediffmail.com>
2020-02-19 09:20:28 -0800

    Add DarkGrey System Theme [2/3]
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- core/java/com/android/internal/util/du/ThemesUtils.java
+++ core/java/com/android/internal/util/du/ThemesUtils.java
@@ -74,6 +74,11 @@ public class ThemesUtils {
             "com.android.theme.bakedgreen.systemui",
     };
 
+    public static final String[] DARK_GREY = {
+            "com.android.theme.darkgrey.system",
+            "com.android.theme.darkgrey.systemui",
+    };
+
     public static final String[] STATUSBAR_ICONS = {
             "com.android.theme.icon_pack.filled.android",
             "com.android.theme.icon_pack.rounded.android",
