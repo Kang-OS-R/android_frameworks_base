2c30a8651935867fb5676356eb04b8352762bea2
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-08-24 22:08:37 +0530

    Don't let DT2S on LS work in Ambient Display
    AshutoshSundresh: Adapted for A11
    
    NotificationPanelView ----> NotificationPanelViewController
    
    Fixes breakage of DT2W on ambient display
    
    (cherry picked from commit a82dd2dcd965cec3b15d747eded1df1dd3f8e98b)
    Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3435,7 +3435,7 @@ public class NotificationPanelViewController extends PanelViewController {
                     mDoubleTapGesture.onTouchEvent(event);
                 }
 
-                if (mIsLockscreenDoubleTapEnabled
+                if (mIsLockscreenDoubleTapEnabled && !mPulsing && !mDozing
                         && mBarState == StatusBarState.KEYGUARD) {
                     mLockscreenDoubleTapToSleep.onTouchEvent(event);
                 }
