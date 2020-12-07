go-find-a-job/platform_frameworks_base

base: Add a toggle for QS footer text
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -134,8 +134,13 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT);
+        int qsFooterValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DERP_FOOTER_TEXT_SHOW, 0,
+                UserHandle.USER_CURRENT);
         MenuItem qsTitlesMenuItem = mToolbar.getMenu().findItem(R.id.menu_item_titles);
+        MenuItem qsFooterText = mToolbar.getMenu().findItem(R.id.footer_text);
         qsTitlesMenuItem.setChecked(qsTitlesValue == 1);
+        qsFooterText.setChecked(qsFooterValue == 1);
 
         mToolbar.setTitleTextColor(accentColor);
         mToolbar.getNavigationIcon().setTint(accentColor);
@@ -329,6 +334,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             item.setChecked(!item.isChecked());
             Settings.System.putIntForUser(mContext.getContentResolver(),
                    Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, item.isChecked() ? 1 : 0,
+                   UserHandle.USER_CURRENT);
+        } else if (id == R.id.footer_text) {
+            item.setChecked(!item.isChecked());
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                   Settings.System.DERP_FOOTER_TEXT_SHOW, item.isChecked() ? 1 : 0,
                    UserHandle.USER_CURRENT);
         } else if (id == R.id.menu_item_rows_one) {
             Settings.System.putIntForUser(mContext.getContentResolver(),

Kang-OS-R/android_frameworks_base

Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -150,8 +150,13 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT);
+        int qsFooterValue = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DERP_FOOTER_TEXT_SHOW, 0,
+                UserHandle.USER_CURRENT);
         MenuItem qsTitlesMenuItem = mToolbar.getMenu().findItem(R.id.menu_item_titles);
+        MenuItem qsFooterText = mToolbar.getMenu().findItem(R.id.footer_text);
         qsTitlesMenuItem.setChecked(qsTitlesValue == 1);
+        qsFooterText.setChecked(qsFooterValue == 1);
 
         mToolbar.setTitleTextColor(accentColor);
         mToolbar.getNavigationIcon().setTint(accentColor);
@@ -382,6 +387,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             item.setChecked(!item.isChecked());
             Settings.System.putIntForUser(mContext.getContentResolver(),
                    Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, item.isChecked() ? 1 : 0,
+                   UserHandle.USER_CURRENT);
+        } else if (id == R.id.footer_text) {
+            item.setChecked(!item.isChecked());
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                   Settings.System.QS_FOOTER_TEXT_SHOW, item.isChecked() ? 1 : 0,
                    UserHandle.USER_CURRENT);
         }
 
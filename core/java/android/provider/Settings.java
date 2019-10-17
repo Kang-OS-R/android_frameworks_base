6e89d909519f52187776e4a9533a8456760ae75f
Ido Ben-Hur <idoybh2@gmail.com>
2019-10-17 04:15:53 +0300

    base: Add a toggle for QS footer text
    Change-Id: If765a1de25141de568d0dc7b8f1be1f1308fd4e6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5723,6 +5723,19 @@ public final class Settings {
         public static final String CHARGING_ANIMATION = "charging_animation";
 
         /**
+         * Whether to show qs footer text
+         * @hide
+         */
+        public static final String QS_FOOTER_TEXT_SHOW = "qs_footer_text_show";
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
