e098da777d7834bf8a8449669d1bf002ec5b0bbd
Alexander Koskovich <zvnexus@outlook.com>
2021-01-18 01:35:42 -0700

    SystemUI: Don't use side paddings if headsUpStartPadding > 0.
     * This dimension was first introduced due to the instability
       of how side paddings, sometimes it'll appear, sometimes
       it won't. In the cases where it does work as intended, now it will
       have that plus the dimension we introduced leading to it going out
       of bounds.
    
    Change-Id: If2f1a8ed98225052a60a21b46d19a71eee26bd04
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/HeadsUpStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/HeadsUpStatusBarView.java
@@ -79,13 +79,20 @@ public class HeadsUpStatusBarView extends AlphaOptimizedLinearLayout {
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         Resources res = getResources();
-        mAbsoluteStartPadding = res.getDimensionPixelSize(R.dimen.notification_side_paddings)
-            + res.getDimensionPixelSize(
-                    com.android.internal.R.dimen.notification_content_margin_start)
-            + res.getDimensionPixelSize(
-	                R.dimen.rounded_corner_content_padding
-            + res.getDimensionPixelSize(
-                    R.dimen.heads_up_status_bar_padding_start);
+        int headsUpStartPadding = getResources().getDimensionPixelSize(R.dimen.heads_up_status_bar_padding_start);
+        if (headsUpStartPadding > 0) {
+            mAbsoluteStartPadding = headsUpStartPadding
+                + res.getDimensionPixelSize(
+                        com.android.internal.R.dimen.notification_content_margin_start)
+                + res.getDimensionPixelSize(
+	                    R.dimen.rounded_corner_content_padding);
+        } else {
+            mAbsoluteStartPadding = res.getDimensionPixelSize(R.dimen.notification_side_paddings)
+                + res.getDimensionPixelSize(
+                        com.android.internal.R.dimen.notification_content_margin_start)
+                + res.getDimensionPixelSize(
+	                    R.dimen.rounded_corner_content_padding);
+        }
         mEndMargin = res.getDimensionPixelSize(
                 com.android.internal.R.dimen.notification_content_margin_end);
         setPaddingRelative(mAbsoluteStartPadding, 0, mEndMargin, 0);
