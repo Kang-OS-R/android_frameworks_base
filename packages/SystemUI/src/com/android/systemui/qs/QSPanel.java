85bd9b40442082da3b221461e4530878f6f0389e
pavanpaps <pavanishot200@gmail.com>
2021-01-24 15:40:00 +0000

    Revert Brightness Slider Stuff
    Moving to OOS 11 QS Layout

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -30,7 +30,6 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.PointF;
-import android.graphics.PorterDuff;
 import android.metrics.LogMaker;
 import android.os.Bundle;
 import android.os.Handler;
@@ -52,7 +51,6 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.UiEventLogger;
@@ -163,7 +161,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private int mFooterMarginStartHorizontal;
     private Consumer<Boolean> mMediaVisibilityChangedListener;
 
-    private View mBrightnessMirror;
 
     @Inject
     public QSPanel(
@@ -192,6 +189,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         setOrientation(VERTICAL);
 
+        addViewsAboveTiles();
         mMovableContentStartIndex = getChildCount();
         mRegularTileLayout = createRegularTileLayout();
 
@@ -219,7 +217,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
             initMediaHostState();
         }
-        addBrightnessView();
         addSecurityFooter();
         if (mRegularTileLayout instanceof PagedTileLayout) {
             mQsTileRevealController = new QSTileRevealController(mContext, this,
@@ -240,16 +237,12 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mSecurityFooter = new QSSecurityFooter(this, mContext);
     }
 
-    protected void addBrightnessView() {
+    protected void addViewsAboveTiles() {
         mBrightnessView = LayoutInflater.from(mContext).inflate(
             R.layout.quick_settings_brightness_dialog, this, false);
-        updateBrightnessViewParams();
         addView(mBrightnessView);
-        final ImageView level = findViewById(R.id.brightness_level);
-        final ImageView icon = findViewById(R.id.brightness_icon);
-        final ToggleSliderView slider = findViewById(R.id.brightness_slider);
-        mBrightnessController = new BrightnessController(getContext(), level, icon,
-                slider, mBroadcastDispatcher);
+        mBrightnessController = new BrightnessController(getContext(),
+                findViewById(R.id.brightness_slider), mBroadcastDispatcher);
     }
 
     protected QSTileLayout createRegularTileLayout() {
@@ -537,8 +530,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             mLastOrientation = newConfig.orientation;
             switchTileLayout();
         }
-
-        updateBrightnessViewParams();
     }
 
     @Override
@@ -663,14 +654,10 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
     }
 
-    public boolean shouldUseHorizontalLayout() {
+    private boolean shouldUseHorizontalLayout() {
         return mUsingMediaPlayer && mMediaHost.getVisible()
                 && getResources().getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
-    }
-
-    boolean isMediaHostVisible() {
-        return mMediaHost.getVisible();
     }
 
     protected void reAttachMediaHost() {
@@ -705,19 +692,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                     .findViewById(R.id.brightness_slider);
             brightnessSlider.setMirror(mirrorSlider);
             brightnessSlider.setMirrorController(mBrightnessMirrorController);
-            if (mBrightnessController != null) {
-                mBrightnessMirror = mBrightnessMirrorController.getMirror();
-                mBrightnessController.setMirrorView(mBrightnessMirror);
-            }
-        }
-    }
-
-    private void updateBrightnessViewParams() {
-        if (mBrightnessView != null) {
-            MarginLayoutParams lp = (MarginLayoutParams) mBrightnessView.getLayoutParams();
-            lp.height = getResources().getDimensionPixelSize(R.dimen.brightness_mirror_height);
-            mBrightnessView.setLayoutParams(lp);
-            forceLayout();
         }
     }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -197,7 +197,6 @@ import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.pulse.VisualizerView;
 import com.android.systemui.qs.QSFragment;
 import com.android.systemui.qs.QSPanel;
-import com.android.systemui.qs.QuickStatusBarHeader;
 import com.android.systemui.statusbar.info.DataUsageView;
 import com.android.systemui.recents.Recents;
 import com.android.systemui.recents.ScreenPinningRequest;
@@ -385,7 +384,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private final VolumeComponent mVolumeComponent;
     private BrightnessMirrorController mBrightnessMirrorController;
-    private BrightnessMirrorController mQuickBrightnessMirrorController;
     private boolean mBrightnessMirrorVisible;
     private BiometricUnlockController mBiometricUnlockController;
     private final LightBarController mLightBarController;
@@ -1316,19 +1314,11 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mBrightnessMirrorVisible = visible;
                         updateScrimController();
                     });
-            mQuickBrightnessMirrorController = new BrightnessMirrorController(
-                    mNotificationShadeWindowView,
-                    mNotificationPanelViewController,
-                    mNotificationShadeDepthControllerLazy.get(),
-                    (visible) -> {
-                        mBrightnessMirrorVisible = visible;
-                        updateScrimController();
-                    });
             fragmentHostManager.addTagListener(QS.TAG, (tag, f) -> {
                 QS qs = (QS) f;
                 if (qs instanceof QSFragment) {
                     mQSPanel = ((QSFragment) qs).getQsPanel();
-                    mQSPanel.setBrightnessMirror(mQuickBrightnessMirrorController);
+                    mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
                 }
             });
         }
@@ -1579,9 +1569,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onDensityOrFontScaleChanged();
         }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onDensityOrFontScaleChanged();
-        }
         // TODO: Bring these out of StatusBar.
         mUserInfoControllerImpl.onDensityOrFontScaleChanged();
         mUserSwitcherController.onDensityOrFontScaleChanged();
@@ -1608,9 +1595,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onOverlayChanged();
-        }
         // We need the new R.id.keyguard_indication_area before recreating
         // mKeyguardIndicationController
         mNotificationPanelViewController.onThemeChanged();
@@ -1621,9 +1605,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void onUiModeChanged() {
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onUiModeChanged();
-        }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.onUiModeChanged();
         }
     }
 
@@ -3505,9 +3486,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.updateResources();
-        }
-        if (mQuickBrightnessMirrorController != null) {
-            mQuickBrightnessMirrorController.updateResources();
         }
     }
 
