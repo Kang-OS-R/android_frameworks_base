c725a2dab67bc056874504b967d6325edd431a3c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-02-08 16:11:45 -0300

    LockIcon: Don't cache icon when scanning face
    Change-Id: I739773e8ac717b782ea8d1f93c3c0bc137d7efa5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
@@ -59,6 +59,7 @@ public class LockIcon extends KeyguardAffordanceView {
     private boolean mKeyguardJustShown;
     private boolean mIsFaceUnlock;
     private boolean mPredrawRegistered;
+    private Drawable mFaceScanningAnim;
     private final SparseArray<Drawable> mDrawableCache = new SparseArray<>();
 
     private final OnPreDrawListener mOnPreDrawListener = new OnPreDrawListener() {
@@ -108,6 +109,7 @@ public class LockIcon extends KeyguardAffordanceView {
 
     public LockIcon(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mFaceScanningAnim = getResources().getDrawable(getThemedAnimationResId(SCANNING));
     }
 
     @Override
@@ -171,6 +173,10 @@ public class LockIcon extends KeyguardAffordanceView {
     }
 
     private Drawable getIcon(int newState) {
+        if (newState == STATE_SCANNING_FACE) {
+            return mFaceScanningAnim;
+        }
+
         @LockAnimIndex final int lockAnimIndex =
                 getAnimationIndexForTransition(mOldState, newState, mPulsing, mDozing,
                         mKeyguardJustShown);
