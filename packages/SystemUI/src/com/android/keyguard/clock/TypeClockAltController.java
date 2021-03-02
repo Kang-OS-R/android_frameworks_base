Octavi-OS/platform_frameworks_base

Keyguard: Make clock widgets great again
@ZeNiXxX
* allow setting accent color for Minutes and hours on every digital clock
* also allow setting accent color for custom clocks

packages/SystemUI/src/com/android/keyguard/clock/TypeClockAltController.java
@@ -24,11 +24,13 @@ import android.graphics.Paint.Style;
 import android.util.MathUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.octavi.OctaviUtils;
 
 import java.util.TimeZone;
 
@@ -84,6 +86,8 @@ public class TypeClockAltController implements ClockPlugin {
      */
     private CrossFadeDarkController mDarkController;
 
+    private Context mContext;
+
     /**
      * Create a TypeClockAltController instance.
      *
@@ -92,10 +96,11 @@ public class TypeClockAltController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     TypeClockAltController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mStatusBarHeight = res.getDimensionPixelSize(R.dimen.status_bar_height);
         mKeyguardLockPadding = res.getDimensionPixelSize(R.dimen.keyguard_lock_padding);
         mKeyguardLockHeight = res.getDimensionPixelSize(R.dimen.keyguard_lock_height);
@@ -185,8 +190,13 @@ public class TypeClockAltController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
+        if(OctaviUtils.useLockscreenCustomClockAccentColor(mContext)) {
+            mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setTextColor(color);
+            mLockClock.setTextColor(color);
+        }
     }
 
     @Override

Kang-OS-R/android_frameworks_base

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/TypeClockAltController.java
@@ -24,11 +24,13 @@ import android.graphics.Paint.Style;
 import android.util.MathUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.content.Context;
 
 import com.android.internal.colorextraction.ColorExtractor;
 import com.android.systemui.R;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.ClockPlugin;
+import com.android.internal.util.du.Utils;
 
 import java.util.TimeZone;
 
@@ -84,6 +86,8 @@ public class TypeClockAltController implements ClockPlugin {
      */
     private CrossFadeDarkController mDarkController;
 
+    private Context mContext;
+
     /**
      * Create a TypeClockAltController instance.
      *
@@ -92,10 +96,11 @@ public class TypeClockAltController implements ClockPlugin {
      * @param colorExtractor Extracts accent color from wallpaper.
      */
     TypeClockAltController(Resources res, LayoutInflater inflater,
-            SysuiColorExtractor colorExtractor) {
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
         mStatusBarHeight = res.getDimensionPixelSize(R.dimen.status_bar_height);
         mKeyguardLockPadding = res.getDimensionPixelSize(R.dimen.keyguard_lock_padding);
         mKeyguardLockHeight = res.getDimensionPixelSize(R.dimen.keyguard_lock_height);
@@ -185,8 +190,13 @@ public class TypeClockAltController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mTypeClock.setTextColor(color);
-        mLockClock.setTextColor(color);
+        if(Utils.useLockscreenCustomClockAccentColor(mContext)) {
+            mTypeClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+            mLockClock.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
+        } else {
+            mTypeClock.setTextColor(color);
+            mLockClock.setTextColor(color);
+        }
     }
 
     @Override
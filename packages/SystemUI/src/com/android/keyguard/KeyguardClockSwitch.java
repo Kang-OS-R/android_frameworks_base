d09c5a1696905cbdc8c6a2fa3eb9d6c6253b67fd
ZeNiXxX <zenixxx.havoc@gmail.com>
2021-01-02 18:30:52 +0000

    Keyguard: Make clock widgets great again
    @ZeNiXxX
    * allow setting accent color for Minutes and hours on every digital clock
    * also allow setting accent color for custom clocks
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -261,7 +261,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
         // Initialize plugin parameters.
         mClockPlugin = plugin;
         mClockPlugin.setStyle(getPaint().getStyle());
-        if(Utils.useLockscreenClockAccentColor(mContext)) {
+        if(Utils.useLockscreenClockHourAccentColor(mContext)) {
             mClockPlugin.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
         } else {
             mClockPlugin.setTextColor(getCurrentTextColor());
@@ -301,7 +301,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
      * It will also update plugin setTextColor if plugin is connected.
      */
     public void setTextColor(int color) {
-        if(Utils.useLockscreenClockAccentColor(mContext)) {
+        if(Utils.useLockscreenClockHourAccentColor(mContext)) {
             mClockView.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             mClockViewBold.setTextColor(mContext.getResources().getColor(R.color.lockscreen_clock_accent_color));
             if (mClockPlugin != null) {
