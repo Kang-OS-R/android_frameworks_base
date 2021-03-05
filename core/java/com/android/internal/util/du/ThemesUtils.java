6c33fe92590213d447ed9eca3cab73ceb30994c0
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-03-05 17:22:28 +0000

    ThemesUtils: add OnePlusSans font [1/3]

--- core/java/com/android/internal/util/du/ThemesUtils.java
+++ core/java/com/android/internal/util/du/ThemesUtils.java
@@ -183,6 +183,7 @@ public class ThemesUtils {
             "com.android.theme.font.linotte",
             "com.android.theme.font.inter",
             "com.android.theme.font.manrope",
+            "com.android.theme.font.opsans",
     };
 
     public static final String[] NAVBAR_STYLES = {
